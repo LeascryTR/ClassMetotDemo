419ebba380d0b442d7aed6681089ac901039d925
Leascry <Leascry@DESKTOP-MLBBGTU>
2021-12-21 21:05:07 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri[] Musteriler)
+        {
+            foreach (var Musteri in Musteriler)
+            {
+                Console.WriteLine("Id : " + Musteri.Id);
+                Console.WriteLine("Müşteri Adı : " + Musteri.Ad);
+                Console.WriteLine("Müşteri Soyadı : " + Musteri.Soyad);
+                Console.WriteLine("Mail Adresi : " + Musteri.Mail);
+                Console.WriteLine("Müşteri Bilgileri Başarılı Şekilde Eklenmiştir. ");
+                Console.WriteLine();
+            }
+        }
+
+        public void Listele(Musteri[] Musteriler)
+        {
+            foreach (var Musteri in Musteriler)
+            {
+                Console.WriteLine("Id : " + Musteri.Id);
+                Console.WriteLine("Müşteri Adı : " + Musteri.Ad);
+                Console.WriteLine("Müşteri Soyadı : " + Musteri.Soyad);
+                Console.WriteLine("Mail Adresi : " + Musteri.Mail);
+                Console.WriteLine("Müşteri Bilgileri Başarılı Şekilde Listelenmiştir. ");
+                Console.WriteLine();
+            }
+        }
+
+        public void Sil(Musteri[] Musteriler)
+        {
+            foreach (var Musteri in Musteriler)
+            {
+
+                Console.WriteLine("Id = " + Musteri.Id + " Müşteri Id li bilgiler Başarılı Şekilde Silinmiştir");
+                Console.WriteLine();
+            }
+        }
+
+        public void Guncelle(Musteri[] Musteriler)
+        {
+            foreach (var Musteri in Musteriler)
+            {
+                Console.WriteLine("Id: " + Musteri.Id);
+                Console.WriteLine("Müşteri Adı: " + Musteri.Ad);
+                Console.WriteLine("Müşteri Soyadı: " + Musteri.Soyad);
+                Console.WriteLine("Mail Adresi: " + Musteri.Mail);
+                Console.WriteLine("Müşteri Bilgileri Başarılı Şekilde Güncellenmiştir. ");
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Emirhan";
+            musteri1.Soyad = "Dönmez";
+            musteri1.Mail = "ed@gmail.com";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Ad = "Oguzgan";
+            musteri2.Soyad = "Dönmez";
+            musteri2.Mail = "od@gmail.com";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 1;
+            musteri3.Ad = "Engin";
+            musteri3.Soyad = "Demiroğ";
+            musteri3.Mail = "ed1@gmail.com";
+
+            Musteri[] Musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager Musteri = new MusteriManager();
+            Musteri.Ekle(Musteriler);
+            Musteri.Listele(Musteriler);
+            Musteri.Sil(Musteriler);
+            Musteri.Guncelle(Musteriler);
+
+        }
+    }
+}
